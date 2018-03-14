4c15780a19e766c29aabf6e8b3aa706768e34d64
Jake Zhang <jakezhang1989@hotmail.com>
2018-03-14 21:55:29 +1100

    cstl_alloc

--- zmdcstl/cstl_alloc.c
+++ zmdcstl/cstl_alloc.c
@@ -18,13 +18,10 @@ void* cstl_align_alloc(const char* allocname, size_t n, size_t alignment, size_t
     assert(pStoredPtr >= p);
     *(pStoredPtr) = p;
 
-    //  if (alignment <= EA_PLATFORM_MIN_MALLOC_ALIGNMENT)
-    // Ensure the result is correctly aligned.  An assertion likely indicates a mismatch between EASTL_ALLOCATOR_MIN_ALIGNMENT and the minimum alignment
+    // Ensure the result is correctly aligned.  An assertion here may indicate a bug in the allocator.
+    // An assertion likely indicates a mismatch between EASTL_ALLOCATOR_MIN_ALIGNMENT and the minimum alignment
     // of EASTLAlloc.  If there is a mismatch it may be necessary to define EASTL_ALLOCATOR_MIN_ALIGNMENT to be the minimum alignment of EASTLAlloc, or
     // to increase the alignment of EASTLAlloc to match EASTL_ALLOCATOR_MIN_ALIGNMENT.
-    //
-    // if (alignment > EA_PLATFORM_MIN_MALLOC_ALIGNMENT)
-    // Ensure the result is correctly aligned.  An assertion here may indicate a bug in the allocator.
     assert(((size_t)(pAligned)& ~(alignment - 1)) == (size_t)(pAligned));
 
     return pAligned;
