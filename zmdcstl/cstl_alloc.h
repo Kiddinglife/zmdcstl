41465ef995cdf5a516d9f8b5b726d14027eebdcd
Jake Zhang <jakezhang1989@hotmail.com>
2018-03-14 21:52:37 +1100

    cstl_alloc

--- zmdcstl/cstl_alloc.h
+++ zmdcstl/cstl_alloc.h
@@ -10,14 +10,6 @@ extern "C"
 {
 #endif
 
-#define CSTL_ALLOCATOR_DEFAULT_NAME "default_cstl_allocator"
-
-    /// allocate_memory
-    ///
-    /// This is a memory allocation dispatching function.
-    /// To do: Make aligned and unaligned specializations.
-    ///        Note that to do this we will need to use a class with a static
-    ///        function instead of a standalone function like below.
     extern void* cstl_align_alloc(const char* allocname, size_t n, size_t alignment, size_t alignmentOffset, int flags);
     extern void* cstl_alloc(const char* allocname, size_t n, int flags);
     extern void  cstl_free(const char* allocname, void* p);
