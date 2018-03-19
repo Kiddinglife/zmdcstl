d90874bd818b1b537d82af821d27586f86cb2dda
kiddinglife <jakezhang1989@hotmail.com>
2018-03-19 21:58:32 +1100

    added new ecipse project fro comparsion with std

---
+++ eclipse/benchmark/benchmark.c
@@ -0,0 +1,13 @@
+#include "greatest.h"
+
+/* Add definitions that need to be in the test runner's main file. */
+GREATEST_MAIN_DEFS()
+;
+
+int main(int argc, char **argv)
+{
+  /* command-line options, initialization. */
+  GREATEST_MAIN_BEGIN();
+
+  GREATEST_MAIN_END(); /* display results */
+}
